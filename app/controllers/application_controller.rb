e266654a659e4035320733ff96921359a26e3c98
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-08-29 13:50:20 +0900

    current_user表示実装１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
+    @user = current_user
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
