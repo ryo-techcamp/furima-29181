dbb19e3162e998302bad777100c7fbb94d53100e
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-08-30 10:54:11 +0900

    ログイン機能実装（成功）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+    redirect_to action::index
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,6 @@ class SessionsController < ApplicationController
   def destroy
   end
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to root_url
-    else
-      render 'new'
-    end
-  end
+ end
+  
 end
