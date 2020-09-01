dfbdde04440cae9e16d1144eb635646975c51542
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-02 00:56:17 +0900

    (商品出品機能)修正１

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,11 +1,9 @@
 function price(){
-  // 画面が読み込まれた時に動くように定義する
    const priceInput = document.getElementById("item-price");
-   const add_tax = document.getElementById("add-tax-price");//販売手数料
-   const profit = document.getElementById("profit");//販売利益
-   // 価格入力時に手数料、利益計算
+   const add_tax = document.getElementById("add-tax-price");
+   const profit = document.getElementById("profit");
      priceInput.addEventListener('keyup', () => {
-         const value = priceInput.value; //value（入力の金額を定義）
+         const value = priceInput.value; 
          
        if (value >= 300 && value <= 9999999){
          let fee = value * 0.1
