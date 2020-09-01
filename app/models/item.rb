a09b10259d3a67b69c5b5ecda1b297a520c30a07
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-02 01:35:06 +0900

    (商品出品機能)単体テストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+validates :image, :name, :description, :category, :status, :delivery_fee, :shipping_origin, :date_of_shipment, :price presence: true
+
+belongs_to :user
+has_one :buyer
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it "imageが空では登録できないこと" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "nameが空では登録できないこと" do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "descriptionが空では登録できないこと" do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "categoryが空では登録できないこと" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "statusが空では登録できないこと" do
+      @item.status = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "delivery_feeが空では登録できないこと" do
+      @item.delivery_fee = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "shipping_originが空では登録できないこと" do
+      @item.shipping_origin = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "date_of_shipmentが空では登録できないこと" do
+      @item.date_of_shipment = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "priceが空では登録できないこと" do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
