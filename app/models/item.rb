2bc7f04336c82b5a0aaae2d69d576b5b80b2a131
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-04 02:00:18 +0900

    整理整頓２

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash [:genre, :sutum, :sora, :place, :day]
 
-validates :image, :name, :description, :genre,:sutum,:sora,:place,:day,  presence: true
+validates :image, :name, :description, :genre_id,:sutum_id,:sora_id,:place_id,:day_id,  presence: true
 
 validates :genre_id,:sutum_id,:sora_id,:place_id,:day_id, numericality: { other_than: 1 } 
 
