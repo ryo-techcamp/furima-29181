05e7e2ea52b2f518af2066cfe7ccbed74f11cc06
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-04 16:06:10 +0900

    出品商品配列整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
   def index
     @user = current_user
+    @item = Item.all
   end
 
   def move_to_index
