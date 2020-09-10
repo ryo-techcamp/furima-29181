e089930f6e9efe40860ca6c7e379591032b316ec
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-10 19:29:47 +0900

    単体テストコード

--- spec/factories/shipping_addres.rb
+++ spec/factories/shipping_addres.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :shipping_addre do
     postal_code { '123-4567' }
-    prefecture_id { '3' }
+    prefecture { '3' }
     city { '八王子市' }
     address { '肉肉1-23' }
-    building { '田中ビル' }
+    building_name { '田中ビル' }
     phone_number { '0901234567' }
     token { '1234' }
 
--- spec/models/shipping_addre_spec.rb
+++ spec/models/shipping_addre_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe ShippingAddre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe BuyerShippingAddress, type: :model do
+  describe '#create' do
+    before do
+      @shipping_address = FactoryBot.build(:shipping_address)
+    end
+
+  it "postal_codeが空では登録できないこと" do
+    @shipping_address.postal_code = nil
+    @shipping_address.valid?
+    expect(@shipping_address.errors.full_messages).to include("postal_code can't be blank")
+  end
+
+  it "prefectureが空では登録できないこと" do
+    @shipping_address.prefecture = nil
+    @shipping_address.valid?
+    expect(@shipping_address.errors.full_messages).to include("prefecture can't be blank")
+  end
+
+  it "city空では登録できないこと" do
+    @shipping_address.city = nil
+    @shipping_address.valid?
+    expect(@shipping_address.errors.full_messages).to include("city can't be blank")
+  end
+
+  it "addressが空では登録できないこと" do
+    @shipping_address.address = nil
+    @shipping_address.valid?
+    expect(@shipping_address.errors.full_messages).to include("address can't be blank")
+  end
+
+  it "building_nameが空では登録できないこと" do
+    @shipping_address.building_name = nil
+    @shipping_address.valid?
+    expect(@shipping_address.errors.full_messages).to include("building_name can't be blank")
+  end
+
+  it "phone_number空では登録できないこと" do
+    @shipping_address.phone_number = nil
+    @shipping_address.valid?
+    expect(@shipping_address.errors.full_messages).to include("phone_number can't be blank")
+  end
 end
